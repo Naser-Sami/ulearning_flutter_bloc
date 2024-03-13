f1443a208de0039a66c78c9657e677d6e37b7e8d
Naser Sami <naser_ebedo@icloud.com>
2024-03-13 18:17:29 +0300

    Sing in bloc

--- lib/pages/sign_in/bloc/sign_in_bloc.dart
+++ lib/pages/sign_in/bloc/sign_in_bloc.dart
@@ -5,14 +5,15 @@ import '/pages/sign_in/bloc/sing_in_event.dart';
 
 class SingInBloc extends Bloc<SingInEvent, SingInState> {
   SingInBloc() : super(const SingInState()) {
+    on<EmailEvent>(_emailEvent);
+    on<PasswordEvent>(_passwordEvent);
+  }
 
-    on<EmailEvent>((event, emil) {
+  void _emailEvent(EmailEvent event, Emitter<SingInState> emit) {
+    emit(state.copyWith(email: event.email));
+  }
 
-    });
-
-    on<PasswordEvent>((event, emil) {
-
-    });
-
+  void _passwordEvent(PasswordEvent event, Emitter<SingInState> emit) {
+    emit(state.copyWith(password: event.password));
   }
 }
